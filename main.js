41797d08753d10448e14816d1e45caaa197d769c
Atsuj <ethanthislair1@gmail.com>
2019-09-27 13:14:51 +0200

    Changement Bot

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
 const { Client } = require("discord.js");
-const client = new Client({disableEveryone: true});
+const { TOKEN, PREFIX } = require("./config");
+const client = new Client();
 
 client.on("ready", () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+  console.log(`Je suis pret !`);
 });
 
 client.on("message", msg => {
-  if (msg.content === "ping") msg.channel.send("Pong!");
-  if (msg.content === "test") msg.channel.send("test");
-  if (msg.content === "wala") msg.channel.send("radimeuh");
+  if (msg.content.startsWith(`${PREFIX}ping`)) msg.channel.send("Pong !");
+  if (msg.content.startsWith(`${PREFIX}pong`)) msg.channel.send("Ping !");
 });
 
-client.login("NjI2NzcwNjY3NDE2ODQ2MzY3.XY3Q7Q.dc3HE52gxaVu9dPt68GlJqymy1E");
+client.login(TOKEN);
